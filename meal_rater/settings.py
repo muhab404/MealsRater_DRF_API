069c933081a61d0b8883e6dbd326553ddf654da4
Muhab Seif <mohab.seif.16@gmail.com>
2022-02-03 20:17:45 +0200

    Deploy to heroku change name

--- meal_rater/settings.py
+++ meal_rater/settings.py
@@ -57,7 +57,7 @@ CORS_ALLOWED_ORIGINS = [
         "http://127.0.0.1:9000"
     ]
 
-ROOT_URLCONF = 'mealrater.urls'
+ROOT_URLCONF = 'meal_rater.urls'
 
 TEMPLATES = [
     {
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mealrater.wsgi.application'
+WSGI_APPLICATION = 'meal_rater.wsgi.application'
 
 
 # Database
